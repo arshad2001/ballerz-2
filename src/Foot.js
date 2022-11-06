d0f906d7743716870d80332b5bf36c8b1b2564ac
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-11-06 21:14:04 +0700

    fonts

--- src/Foot.js
+++ src/Foot.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import './index.css'
+import './foot.css'
 
 function Foot() {
 	return (
-		<div className='footer-background'>
+		<div className='footer-background font-color-white'>
 			<footer className="footer">
 				<div class="footerSection">
-					<a>Ballin</a>
+					<a className='font-weight-bold'>HIMATEKKOM</a>
 				</div>
 				<div class="footerSection">
-					<a>Ballin</a>
+					<a className=''>Follow Us On...</a>
 				</div>
 				<div class="footerSection">
-					<a>Ballin</a>
+					<a className='font-weight-light'>2022 | RELCOM CONVERGENCE HIMATEKKOM ITS</a>
 				</div>
 			</footer>
 		</div>
