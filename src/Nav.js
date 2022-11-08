cd1fc5b1a599824fb55229ad6dc0a8464bc522fa
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-11-08 10:59:18 +0700

    updated navbar layout and fonts from feedback

--- src/Nav.js
+++ src/Nav.js
@@ -10,10 +10,11 @@ function Nav() {
 		<div>
 			<nav class="navbar background ">
 				<div className='nav-section'>
-					<img src={logoColored} className='image-size-52 margin-8 logo-hima-navbar'/>
+					<img src={logoColored} className='image-size-32 margin-8 logo-hima-navbar'/>
+					<a className='font-size-20 font-weight-bold'>HIMATEKKOM ITS</a>
 					{/* <a className='font-size-16 font-weight-bold'>HIMATEKKOM</a> */}
 				</div>
-				<ul class="nav-list font-size-20 font-weight-medium">
+				<ul class="nav-list font-size-20 font-weight-normal">
 					<li><a href="#Home">Home</a></li>
 					<li><a href="#Info">Info</a></li>
 					<li><a href="#Academic">Academic</a></li>
