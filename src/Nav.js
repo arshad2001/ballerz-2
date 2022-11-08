e654fb47ad8a9f2f8e9c4ced240d1fb36e9939ad
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-11-08 09:54:19 +0700

    small changes

--- src/Nav.js
+++ src/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
 		<div>
 			<nav class="navbar background ">
 				<div className='nav-section'>
-					<img src={logoColored} className='image-size-64 margin-8 logo-hima-navbar'/>
+					<img src={logoColored} className='image-size-52 margin-8 logo-hima-navbar'/>
 					{/* <a className='font-size-16 font-weight-bold'>HIMATEKKOM</a> */}
 				</div>
 				<ul class="nav-list font-size-20 font-weight-medium">
