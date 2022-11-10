36220d1a150f49e0fd34f4eb5355d12f5d5bc09b
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-11-10 22:29:32 +0700

    n

--- src/App.js
+++ src/App.js
@@ -6,91 +6,92 @@ import Foot from "./Foot";
 import Card from "./Card";
 
 const cardContents = [
-  {
-    title: "Machine Learning",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
-  {
-    title: "The Coldest Sunset",
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.`,
-  },
+	{
+		title: "Machine Learning",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
+	{
+		title: "The Coldest Sunset",
+		content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Voluptatibus quia, nulla! Maiores et perferendis eaque,
+				exercitationem praesentium nihil.`,
+	},
 ];
 
 function App() {
-  return (
-    <>
-      <Nav />
-      <div>
-        <section class="section place-content-center py-20 px-80">
-          <div class="grid grid-cols-3">
-            {cardContents.map(({ title, content }) => (
-              <Card title={title} content={content} />
-            ))}
-          </div>
-        </section>
-      </div>
-      <Foot />
-    </>
-  );
+	return (
+		<>
+		<Nav />
+		<div>
+			<section class="section place-content-center py-20 px-80">
+			<div class="grid grid-cols-3">
+				{cardContents.map(({ title, content }) => (
+				<Card title={title} content={content} />
+				))}
+			</div>
+			</section>
+		</div>
+		<Foot />
+		</>
+	);
 }
-//make the app
+//I think we need to make the app as a segment for the page itself
+//only then the app can contain the cards
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -4,10 +4,10 @@ import "./card.css";
 function Card({ title, content }) {
   return (
     <div>
-      <div class="max-w-sm overflow-hidden border border-sky-500">
+      <div class="max-w-sm overflow-hidden border border-blue-500 hover:bg-blue-500 hover:text-white">
         <div class="px-8 py-8">
           <div class="font-bold text-xl mb-2">{title}</div>
-          <p class="text-gray-700 text-base">{content}</p>
+          <p class="text-base">{content}</p>
         </div>
       </div>
     </div>
