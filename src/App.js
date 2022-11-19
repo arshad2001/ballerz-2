6940edc451a30bc2aacf21642b6861bba7ad6cca
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-11-19 21:42:26 +0700

    n

--- src/App.js
+++ src/App.js
@@ -11,46 +11,55 @@ const cardContents = [
     title: "CONVERGENCE",
     content: "CONVERGENCE",
     image: "./convergence-blue.png",
+    hover: "./convergence-blue.png",
   },
   {
     title: "THE EXECUTIVE BOARD",
     content: "CONVERGENCE",
     image: "./exec-blue.png",
+    hover: "./exec-blue.png",
   },
   {
     title: "TIMELINE",
     content: "CONVERGENCE",
     image: "./convergence-blue.png",
+    hover: "./convergence-blue.png",
   },
   {
     title: "STUDENT WELFARE",
     content: "CONVERGENCE",
     image: "./studfare-blue.png",
+    hover: "./studfare-blue.png",
   },
   {
     title: "RELATIONS & COMMUNICATIONS ",
     content: "CONVERGENCE",
     image: "./relcom-blue.png",
+    hover: "./relcom-blue.png",
   },
   {
     title: "INTERNAL AFFAIRS",
     content: "CONVERGENCE",
     image: "./internal-blue.png",
+    hover: "./internal-blue.png",
   },
   {
     title: "HUMAN RESOURCES",
     content: "CONVERGENCE",
     image: "./hrd-blue.png",
+    hover: "./hrd-blue.png",
   },
   {
     title: "ENTREPRENEURSHIP",
     content: "CONVERGENCE",
     image: "./entr-blue.png",
+    hover: "./entr-blue.png",
   },
   {
     title: "RESEARCH & PROFESSION",
     content: "CONVERGENCE",
     image: "./respro-blue.png",
+    hover: "./respro-blue.png",
   },
 ];
 
@@ -67,7 +76,7 @@ function App() {
   return (
     <div className="">
       <Nav />
-      <div className="pt-20 pb-8">
+      <div className="pt-24 pb-8">
         <section class=" flex items-center content-between place-content-center">
           <div class="bg-custom-white  grid grid-cols-3">
             {cardContents.map(({ title, content, image }) => (
@@ -75,7 +84,6 @@ function App() {
             ))}
           </div>
         </section>
-        <div></div>
       </div>
       <Foot />
     </div>
--- src/Card.js
+++ src/Card.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./card.css";
 import image from "./assets/convergence-blue.png";
 
-function Card({ title, content, image }) {
+function Card({ title, content, image, hover }) {
+  function changeBackground(e) {
+    e.target.style.background = `url(${image})`;
+  }
+
+  //div line 12 kasih ini onMouseOver={changeBackground}
   return (
     <div>
       <div
         style={{
           backgroundImage: `url(${image})`,
         }}
-        className="bg-right-top h-[270px] w-[360px] hover:scale-105 hover:shadow-2xl overflow-hidden border border-custom-blue 
-        hover:bg-custom-blue hover:text-white transition duration-300 ease-in-out event cursor-pointer"
+        className="bg-[top_.5px_left_-.5px] bg-contain bg-no-repeat h-[200px] w-[280px] hover:scale-105 hover:shadow-2xl overflow-hidden border
+        border-custom-blue hover:text-white hover:bg-custom-blue
+        transition duration-300 ease-in-out event cursor-pointer"
       >
         <div class="px-4 py-4">
-          <div class="text-[12px]">{content}</div>
-          <p class="font-bold text-[24px] mb-4 ">{title}</p>
+          <div class="text-[10px]">{content}</div>
+          <p class="font-bold text-[20px] mb-4 ">{title}</p>
           {/* <img src="/assets/logo-colored.png" /> */}
         </div>
       </div>
